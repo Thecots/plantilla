1654e2a9a4f968b4a11248fe2fb0a53382cb361d
thecots cots <thecots22@gmail.com>
2022-03-24 12:39:02 +0100

    asd

---
+++ db/connector.php
@@ -0,0 +1,1 @@
+<?php $dblink=mysqli_connect("127.0.0.1","root","","borsa") or exit(mysqli_connect_error()); ?>
---
+++ script.js
@@ -0,0 +1,4 @@
+/* alertas success / error */
+function _alert(icon,title,text){
+  Swal.fire({icon,title,text,})
+}
