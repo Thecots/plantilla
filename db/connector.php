2e08455d30c23adf791e41b8325463a954028953
thecots cots <thecots22@gmail.com>
2022-03-25 13:36:38 +0100

    asd

--- db/connector.php
+++ db/connector.php
@@ -1,1 +1,1 @@
-<?php $dblink=mysqli_connect("127.0.0.1","root","","borsa") or exit(mysqli_connect_error()); ?>
+<?php $dblink=mysqli_connect("127.0.0.1","root","root","xxxx") or exit(mysqli_connect_error()); ?>
